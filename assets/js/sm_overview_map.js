dd1a69a6513a38fa49a47ef2f4270a204c3308e3
denny4_user <82503296+denny4-user@users.noreply.github.com>
2025-05-19 08:13:10 +0300

    Update sm_overview_map.js

--- assets/js/sm_overview_map.js
+++ assets/js/sm_overview_map.js
@@ -10,9 +10,9 @@ SMOverviewMap = (function() {
     var map;
     var clickmarker;
 
-    var minZoom = -1
+    var minZoom = 0
     var maxZoom = 5
-    var gridSize = 128
+    var gridSize = 64
 
     // A quick extension to allow image layer rotation.
     L.RotateImageLayer = L.ImageOverlay.extend({
